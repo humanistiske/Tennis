c830661550cf9135930fae9e208ce6026ee09ad8
Ganesh Chawkekar <gcool5656@gmail.com>
2018-05-22 11:23:41 +0530

    Tennis Applet

---
+++ src/Tennis/Paddle.java
@@ -0,0 +1,11 @@
+package Tennis;
+
+import java.awt.Graphics;
+
+public interface Paddle 
+{
+	public void draw(Graphics g);
+	public void move();
+	public int getY();
+	
+}
---
+++ src/Tennis/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author GaneshC
+ *
+ */
+package Tennis;
